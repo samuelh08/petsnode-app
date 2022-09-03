c24e9867b470ce23fec44b647c7e1f87ac1e3232
Samuel <samuelh08.shm@gmail.com>
2022-09-02 19:57:21 -0500

    Refactor login box with grid

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,13 +2,15 @@ import React, { useContext, useState } from 'react';
 import { useRouter } from 'next/router';
 import {
   Alert,
-  Box,
   Button,
   FormControl,
+  Grid,
   Input,
   InputLabel,
   Paper,
+  Typography,
 } from '@mui/material';
+import PetsIcon from '@mui/icons-material/Pets';
 
 import { logIn } from '../../api/users';
 import UserContext from '../../context/user';
@@ -40,30 +42,53 @@ export default function Login() {
   return (
     <>
       {error && <Alert severity="error">{error}</Alert>}
-      <Box maxWidth="330px" margin="0 auto">
-        <Paper elevation={10}>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            display="flex"
-            flexDirection="column"
-            padding="20px"
-            marginTop="30px"
-          >
-            <FormControl>
-              <InputLabel htmlFor="email">Email address</InputLabel>
-              <Input id="email" />
-            </FormControl>
-            <FormControl>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input type="password" id="password" />
-            </FormControl>
-            <Button variant="contained" type="submit" loading={loading}>
-              Login
-            </Button>
-          </Box>
-        </Paper>
-      </Box>
+
+      <Paper elevation={10} sx={{ margin: '100px auto', maxWidth: '330px' }}>
+        <Grid container flexDirection="column" alignItems="center">
+          <Grid item>
+            <Grid container justifyContent="center" padding="20px">
+              <Grid item marginRight={1}>
+                <PetsIcon />
+              </Grid>
+              <Grid item>
+                <Typography variant="h5">PetsNode</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="body">Log in to your account</Typography>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              component="form"
+              onSubmit={handleSubmit}
+              flexDirection="column"
+              alignItems="center"
+              padding="20px"
+              spacing={2}
+            >
+              <Grid item>
+                <FormControl>
+                  <InputLabel htmlFor="email">Email address</InputLabel>
+                  <Input id="email" />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input type="password" id="password" />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" type="submit" loading={loading}>
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </>
   );
 }
